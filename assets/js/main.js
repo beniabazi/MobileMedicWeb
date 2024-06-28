03147014ee1a24eaab23e185a4adfeda1a488e4d
Beni Abazi <benabazi@gmail.com>
2024-06-28 13:14:33 +1000

    On branch main  Changes to be committed: 	new file:   .gitignore 	modified:   assets/css/styles.css 	new file:   assets/img/apple_logo.jpg 	new file:   assets/img/battery_low.png 	new file:   assets/img/care-chips.png 	new file:   assets/img/cheese.png 	new file:   assets/img/chips-1.png 	new file:   assets/img/chips-2.png 	new file:   assets/img/chips-3.png 	new file:   assets/img/contact-man.png 	new file:   assets/img/crab.png 	new file:   assets/img/customer.jpeg 	new file:   assets/img/customer.jpg 	new file:   assets/img/favorite-chips-1.png 	new file:   assets/img/favorite-chips-2.png 	new file:   assets/img/favorite-chips-3.png 	new file:   assets/img/google_repair.jpeg 	new file:   assets/img/hamburger.png 	new file:   assets/img/home-chips.png 	new file:   assets/img/iphone_repair.jpeg 	new file:   assets/img/leaf-1.png 	new file:   assets/img/leaf-2.png 	new file:   assets/img/leaf-3.png 	new file:   assets/img/meat.png 	new file:   assets/img/product-chips-1.png 	new file:   assets/img/product-chips-2.png 	new file:   assets/img/product-chips-3.png 	new file:   assets/img/product-chips-4.png 	new file:   assets/img/product-chips-5.png 	new file:   assets/img/samsung_logo.jpg 	new file:   assets/img/samsung_repair.jpg 	new file:   assets/img/shrimp.png 	new file:   assets/img/tomato-1.png 	new file:   assets/img/tomato-2.png 	modified:   assets/js/main.js 	new file:   get_quote.html 	new file:   images/1 - Copy - Copy.png 	new file:   images/1 - Copy.png 	new file:   images/1 white.png 	new file:   images/1.png 	new file:   images/1black.png 	new file:   images/2.png 	new file:   images/Invoice Template.docx 	new file:   images/Screenshot 2024-06-12 230608.png 	new file:   images/Screenshot 2024-06-12 230754.png 	new file:   images/Your paragraph text.png 	new file:   images/logo no bg.png 	new file:   images/white-removebg-preview.png 	new file:   images/white.png 	new file:   images/whitelogo.png 	modified:   index.html 	new file:   mobile_medic_web/README.md 	new file:   mobile_medic_web/Text Responsive chips website.txt 	new file:   mobile_medic_web/preview.png 	new file:   style/style.css

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,149 +1,300 @@
 /*=============== SHOW MENU ===============*/
-const navMenu = document.getElementById('nav-menu'),
-      navToggle = document.getElementById('nav-toggle'),
-      navClose = document.getElementById('nav-close')
+const navMenu = document.getElementById("nav-menu"),
+  navToggle = document.getElementById("nav-toggle"),
+  navClose = document.getElementById("nav-close");
 
-if(navToggle){
-    navToggle.addEventListener('click', () => {
-        navMenu.classList.add('show-menu')
-    });
+if (navToggle) {
+  navToggle.addEventListener("click", () => {
+    navMenu.classList.add("show-menu");
+  });
 }
 
-if(navClose){
-    navClose.addEventListener('click', () => {
-        navMenu.classList.remove('show-menu')
-    })
+if (navClose) {
+  navClose.addEventListener("click", () => {
+    navMenu.classList.remove("show-menu");
+  });
 }
 
 /*=============== REMOVE MENU MOBILE ===============*/
-const navLink = document.querySelectorAll('.nav__link')
+const navLink = document.querySelectorAll(".nav__link");
 
 const linkAction = () => {
-    const navMenu = document.getElementById('nav-menu')
-    navMenu.classList.remove('show-menu')
-}
-navLink.forEach(n => n.addEventListener('click', linkAction))
+  const navMenu = document.getElementById("nav-menu");
+  navMenu.classList.remove("show-menu");
+};
+navLink.forEach((n) => n.addEventListener("click", linkAction));
 
 /*=============== SHADOW HEADER ===============*/
-const shadowHeader = () =>{
-    const header = document.getElementById('header')
-    this.scrollY >= 50 ? header.classList.add('shadow-header')
-    : header.classList.remove('shadow-header')
-}
-window.addEventListener('scroll', shadowHeader)
+const shadowHeader = () => {
+  const header = document.getElementById("header");
+  this.scrollY >= 50
+    ? header.classList.add("shadow-header")
+    : header.classList.remove("shadow-header");
+};
+window.addEventListener("scroll", shadowHeader);
 
-/*=============== SWIPER FAVORITES ===============*/ 
-const swiperFavorites = new Swiper('.favorites__swiper', {
-    loop: true,
-    grabCursor: true,
-    slidesPerView: 'auto',
-    centeredSlides: 'auto'
-  })
+/*=============== SWIPER FAVORITES ===============*/
+const swiperFavorites = new Swiper(".favorites__swiper", {
+  loop: true,
+  grabCursor: true,
+  slidesPerView: "auto",
+  centeredSlides: "auto",
+});
 
-/*=============== SHOW SCROLL UP ===============*/ 
-const scrollUp = () =>{
-    const scrollUp = document.getElementById('scroll-up')
-    this.scrollY >= 350 ? scrollUp.classList.add('show-scroll')
-    : scrollUp.classList.remove('show-scroll')
-}
-window.addEventListener('scroll', scrollUp)
+/*=============== SHOW SCROLL UP ===============*/
+const scrollUp = () => {
+  const scrollUp = document.getElementById("scroll-up");
+  this.scrollY >= 350
+    ? scrollUp.classList.add("show-scroll")
+    : scrollUp.classList.remove("show-scroll");
+};
+window.addEventListener("scroll", scrollUp);
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
-const sections = document.querySelectorAll('section[id]')
+const sections = document.querySelectorAll("section[id]");
 
 const scrollActive = () => {
-    const scrollDown = window.scrollY
+  const scrollDown = window.scrollY;
 
-    sections.forEach(section => {
-        const sectionHeight = current.offsetHeight,
-        sectionTop = current.offsetTop - 58,
-        sectionId = current.getAttribute('id'),
-        sectionsClass = document.querySelector('.nav__menu a[href*=' + sectionId + ']')
+  sections.forEach((current) => {
+    const sectionHeight = current.offsetHeight,
+      sectionTop = current.offsetTop - 58,
+      sectionId = current.getAttribute("id"),
+      sectionsClass = document.querySelector(
+        ".nav__menu a[href*=" + sectionId + "]"
+      );
 
-        if(scrollDown > sectionTop && scrollDown <= sectionTop + sectionHeight){
-            sectionsClass.classList.add('active-link')
-        }else{
-            sectionsClass.classList.remove('active-link')
-        }
-    })
-}
+    if (scrollDown > sectionTop && scrollDown <= sectionTop + sectionHeight) {
+      sectionsClass.classList.add("active-link");
+    } else {
+      sectionsClass.classList.remove("active-link");
+    }
+  });
+};
+
+window.addEventListener("scroll", scrollActive);
 
 /*=============== SCROLL REVEAL ANIMATION ===============*/
+const sr = ScrollReveal({
+  origin: "top",
+  distance: "60px",
+  duration: 2500,
+  delay: 300,
+  reset: true,
+});
 
-
-
-
-
+sr.reveal(
+  ".home__data, .favourites__containner, .footer__container, .home__buttons"
+);
+sr.reveal(".home__circle, .home__image", { delay: 600, scale: 0.5 });
+sr.reveal(".care__img, .contact__img", { origin: "left" });
+sr.reveal(".care__list, .contact__data", { origin: "right" });
+sr.reveal(".banner__item", { interval: "100" });
 
 /* ======== Pop Up Quote ================= */
 
-const openQuoteButtons = document.querySelectorAll('[data-quote-target]')
-const closeQuoteButtons = document.querySelectorAll('[data-close-button]')
+const openQuoteButtons = document.querySelectorAll("[data-quote-target]");
+const closeQuoteButtons = document.querySelectorAll("[data-close-button]");
 
-const openRepairButtons = document.querySelectorAll('[data-quote-target]')
-const closeRepairButtons = document.querySelectorAll('[data-close-button]')
-const overlay =document.getElementById('overlay')
+const openRepairButtons = document.querySelectorAll("[data-quote-target]");
+const closeRepairButtons = document.querySelectorAll("[data-close-button]");
+const overlay = document.getElementById("overlay");
 
+openQuoteButtons.forEach((button) => {
+  button.addEventListener("click", () => {
+    const quote = document.querySelector(button.dataset.quoteTarget);
+    openQuote(quote);
+  });
+});
+closeQuoteButtons.forEach((button) => {
+  button.addEventListener("click", () => {
+    const quote = button.closest(".quote");
+    closeQuote(quote);
+  });
+});
+openRepairButtons.forEach((button) => {
+  button.addEventListener("click", () => {
+    const repair = document.querySelector(button.dataset.repairTarget);
+    openRepair(repair);
+  });
+});
+closeRepairButtons.forEach((button) => {
+  button.addEventListener("click", () => {
+    const repair = button.closest(".repair");
+    closeRepair(repair);
+  });
+});
 
-
-openQuoteButtons.forEach(button => {
-    button.addEventListener('click', () => {
-        const quote = document.querySelector(button.dataset.quoteTarget)
-        openQuote(quote)
-    })
-})
-closeQuoteButtons.forEach(button => {
-    button.addEventListener('click', () => {
-        const quote = button.closest('.quote')
-        closeQuote(quote)
-    })
-})
-openRepairButtons.forEach(button => {
-    button.addEventListener('click', () => {
-        const repair = document.querySelector(button.dataset.repairTarget)
-        openRepair(repair)
-    })
-})
-closeRepairButtons.forEach(button => {
-    button.addEventListener('click', () => {
-        const repair = button.closest('.repair')
-        closeRepair(repair)
-    })
-})
-
-overlay.addEventListener('click', () => {
-    const quote = document.querySelectorAll('.quote.active')
-    quote.forEach(quote => {
-        closeQuote(quote)
-    })
-})
-overlay.addEventListener('click', () => {
-    const repair = document.querySelectorAll('.repair.active')
-    repair.forEach(repair => {
-        closeRepair(repair)
-    })
-})
-
+overlay.addEventListener("click", () => {
+  const quote = document.querySelectorAll(".quote.active");
+  quote.forEach((quote) => {
+    closeQuote(quote);
+  });
+});
+overlay.addEventListener("click", () => {
+  const repair = document.querySelectorAll(".repair.active");
+  repair.forEach((repair) => {
+    closeRepair(repair);
+  });
+});
 
 function openQuote(quote) {
-    if (quote == null) return
-    quote.classList.add('active')
-    overlay.classList.add('active')
+  if (quote == null) return;
+  quote.classList.add("active");
+  overlay.classList.add("active");
 }
 function closeQuote(quote) {
-    if (quote == null) return
-    quote.classList.remove('active')
-    overlay.classList.remove('active')
+  if (quote == null) return;
+  quote.classList.remove("active");
+  overlay.classList.remove("active");
 }
 function openRepair(repair) {
-    if (repair == null) return
-    repair.classList.add('active')
-    overlay.classList.add('active')
+  if (repair == null) return;
+  repair.classList.add("active");
+  overlay.classList.add("active");
 }
 function closeRepair(repair) {
-    if (repair == null) return
-    repair.classList.remove('active')
-    overlay.classList.remove('active')
+  if (repair == null) return;
+  repair.classList.remove("active");
+  overlay.classList.remove("active");
 }
 
-/* ======== Pop Up Repair ================= */
+/* ======== Pop Up Quote Selector ================= */
+
+var countryStateCityinfo = {
+  Phone: {
+    "Apple": {
+      "iPhone X": ["AfterMarket(Soft OLED) Screen = $169",
+        "Back Glass = $99",
+        "Battery Replacement = $99",
+        "Back Camera = $149",
+        "Charging Port = $119"],
+
+      "iPhon_XS": ["AfterMarket(Soft OLED) Screen = $169",
+        "Back Glass = $99",
+        "Battery Replacement = $99",
+        "Back Camera = $149",
+        "Charging Port = $119"],
+     
+      iPhone_15_Pro_MAX: ["6429", "6432"]
+    },
+    Samsung: {
+      Hobart: ["7000", "7520"],
+      Burnie: ["7320", "7315"]
+    },
+  },
+
+
+
+
+  Tablet: {
+    Bavaria: {
+      Munich: ["80331", "80333", "80335"],
+      Numemberg: ["90402", "90403", "90404"]
+    }
+  }
+
+
+}
+
+window.onload = function(){
+  const selectCountry = document.getElementById('country'),
+  selectState = document.getElementById('state'),
+  selectCity = document.getElementById('city'),
+  selectZip = document.getElementById('zip'),
+  selects = document.querySelectorAll('select')
+
+  selectState.disabled = true
+  selectCity.disabled = true
+  selectZip.disabled = true
+
+  selects.forEach(select => {
+    if(select.disabled == true){
+      select.style.cursor = "auto"
+    }
+    else{
+      select.style.cursor = "pointer"
+    }
+  })
+
+
+  //drop down functions
+for(let country in countryStateCityinfo){
+  /* console.log(country); */
+  selectCountry.options[selectCountry.options.length] = new Option(country, country)
+}
+
+//country change
+selectCountry.onchange = (e) =>{
+  selectState.disabled = false
+  selectCity.disabled = true
+  selectZip.disabled = true
+
+  selects.forEach(select => {
+    if(select.disabled == true){
+      select.style.cursor = "auto"
+    }
+    else{
+      select.style.cursor = "pointer"
+    }
+  })
+
+  selectState.length = 1
+  selectCity.length = 1
+  selectZip.length = 1
+  
+  for(let state in countryStateCityinfo[e.target.value]){
+    console.log(state);
+    selectState.options[selectState.options.length] = new Option(state, state)
+  }
+
+
+}
+
+//state change
+selectState.onchange = (e) =>{
+  selectCity.disabled = false
+  selectZip.disabled = true
+
+  selects.forEach(select => {
+    if(select.disabled == true){
+      select.style.cursor = "auto"
+    }
+    else{
+      select.style.cursor = "pointer"
+    }
+  })
+
+  selectCity.length = 1
+  selectZip.length = 1
+
+  for(let city in countryStateCityinfo[selectCountry.value][e.target.value]){
+    console.log(city);
+    selectCity.options[selectCity.options.length] = new Option(city, city)
+  }
+}
+
+//city change
+selectCity.onchange = (e) =>{
+selectZip.disabled = false
+
+selects.forEach(select => {
+  if(select.disabled == true){
+    select.style.cursor = "auto"
+  }
+  else{
+    select.style.cursor = "pointer"
+  }
+})
+
+selectZip.length = 1
+
+let zips = countryStateCityinfo[selectCountry.value][selectState.value][e.target.value]
+
+for(let i=0; i<zips.length; i++){
+  selectZip.options[selectZip.options.length] = new Option(zips[i], zips[i])
+}
+}
+
+}
+
